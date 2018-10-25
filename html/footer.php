4afc9dfa947d50e738f03887a271f90d01134420
kyreeniaa0403 <9513.joan23@fje.edu>
2018-10-25 17:48:05 +0200

    html recursos

---
+++ html/footer.php
@@ -0,0 +1,6 @@
+    <div class="footer">
+        <p>PROYECTO 01 - DAW2:</p>
+        <p>Ernest Azanza, Daniel Soriano y Kyrenia Muñoz.</p>
+    </div>
+  </body>
+</html>
---
+++ html/header.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+      <title>PROYECTO 01</title>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="../css/header.css" type="text/css" media="all">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+      <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+      <link rel="icon" type="image/png" href="../img/logo.png">
+  </header>
+  <body>
+
+      <div class="header">
+        <div class="titulo-header text-center">
+            <h1>RECURSOS</h1>
+            <p>¿Qué recurso quieres reservar?</p>
+        </div>
+      </div>
